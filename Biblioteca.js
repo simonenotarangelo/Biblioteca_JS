5ef5c61a3bd442981e1bec17cbf224bbb85d2667
Simoncarlo Notarangelo <simone.notara@gmail.com>
2024-05-13 17:22:59 +0200

    Aggiunta oggetti per i libri

--- Biblioteca.js
+++ Biblioteca.js
@@ -1,8 +1,50 @@
 const prompt=require('prompt-sync')();
 
-function aggiuntaLibro()
-{
+let Novecento = {
 
+    autore:"Alessandro Barrico",
+    genere:"Romanzo",
+    isbn:"978-88-123456-1"
+}
+
+let Lezioni = {
+
+    autore:"Italo Calvino",
+    genere:"Saggio",
+    isbn:"978-88-123456-2"
+}
+
+let Solaria = {
+
+    autore:"Carmine Abate",
+    genere:"Romanzo",
+    isbn:"978-88-123456-3"
+}
+
+let Isola = {
+
+    autore:"Aldous Huxley",
+    genere:"Fantascienza",
+    isbn:"978-88-123456-4"
+}
+
+let Arcipelago = {
+
+    autore:"Tiziano Scarpa",
+    genere:"Romanzo",
+    isbn:"978-88-123456-5"
+}
+
+let Inferno = {
+
+    autore:"Dan Brown",
+    genere:"Thriller",
+    isbn:"978-88-123456-6"
+}
+
+function aggiornamentiLibro()
+{
+    
 }
 
 function visualizzaLibri()
@@ -16,7 +58,7 @@ function main()
 
     console.log("Benvenuto nella Biblioteca. Cosa desideri fare?");
 
-    console.log("1)Aggiungi libro");
+    console.log("1)Aggiorna informazioni di un libro");
     console.log("2)Visualizza lista completa dei libri");
     console.log("0)Chiudi programma");
 
@@ -27,7 +69,7 @@ function main()
         switch(scelta)
         {
             case 0:break;
-            case 1:aggiuntaLibro();break;
+            case 1:aggiornamentiLibro();break;
             case 2:visualizzaLibri();break;
         }
 
