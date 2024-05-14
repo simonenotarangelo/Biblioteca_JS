51b62b0ea9fd387301d300f261eae66d6d1e13fa
Andrea0183 <turcoandrea64@gmail.com>
2024-05-14 16:29:53 +0200

    Inizio funzione per la stampa dei libri

--- Biblioteca.js
+++ Biblioteca.js
@@ -1,7 +1,10 @@
 const prompt=require('prompt-sync')();
 
+//cercare un metodo per salvare i cambiamenti (es. se un utente prende in prestito un libro, anche se chiudiamo il programma, questo prestito deve rimanere registrato).
+
 let Novecento = {
 
+    titolo: "Novecento",
     autore:"Alessandro Barrico",
     genere:"Romanzo",
     isbn:"978-88-123456-1"
@@ -9,27 +12,31 @@ let Novecento = {
 
 let Lezioni = {
 
+    titolo: "Lezioni",
     autore:"Italo Calvino",
     genere:"Saggio",
     isbn:"978-88-123456-2"
 }
 
 let Solaria = {
-
+    
+    titolo:"Solaria",
     autore:"Carmine Abate",
     genere:"Romanzo",
     isbn:"978-88-123456-3"
 }
 
 let Isola = {
-
+    
+    titolo: "Isola",
     autore:"Aldous Huxley",
     genere:"Fantascienza",
     isbn:"978-88-123456-4"
 }
 
 let Arcipelago = {
-
+    
+    titolo: "Arcipelago",
     autore:"Tiziano Scarpa",
     genere:"Romanzo",
     isbn:"978-88-123456-5"
@@ -37,6 +44,7 @@ let Arcipelago = {
 
 let Inferno = {
 
+    titolo: "Inferno",
     autore:"Dan Brown",
     genere:"Thriller",
     isbn:"978-88-123456-6"
@@ -44,11 +52,16 @@ let Inferno = {
 
 function aggiornamentiLibro()
 {
+
     
 }
 
 function visualizzaLibri()
 {
+    //switch case visualizzazione totale, per nome ecc...
+    console.log("Ecco la lista dei libri presenti nella Biblioteca");
+
+    console.log(Novecento + "\n" + Lezioni + "\n" +Solaria + "\n" + Isola + "\n" + Arcipelago + "\n" + Inferno);
     
 }
 
