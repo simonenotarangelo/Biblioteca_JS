b8bf5c19714c9556542f4978d039911af910eb24
Massimo Lotti <lottimassimo1@gmail.com>
2024-05-26 18:58:49 +0200

    Documentazione Avanzata
    Inizio documentazione JsDoc sul documento avanzato.
    Creazione di un nuovo file ("Documentazione_Avanzata"), che contiene gia il codice terminato con i commenti

---
+++ documentazione/src/Documentazione_Avanzata.js
@@ -0,0 +1,391 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const prompt = require('prompt-sync')();
+
+/**
+ * @classdesc Rappresenta un libro con le sue informazioni di base.
+ * @class
+ */
+class Libro {
+    /**
+     * Crea un'istanza di Libro.
+     * @constructor
+     * @param {string} titolo - Il titolo del libro.
+     * @param {string} autore - L'autore del libro.
+     * @param {string} genere - Il genere del libro.
+     * @param {string} isbn - L'ISBN (International Standard Book Number) del libro.
+     */
+    constructor(titolo, autore, genere, isbn) {
+      this.titolo = titolo; // Il titolo del libro.
+      this.autore = autore; // L'autore del libro.
+      this.genere = genere; // Il genere del libro.
+      this.isbn = isbn; // L'ISBN del libro.
+    }
+  
+    /**
+     * Restituisce una rappresentazione testuale del libro.
+     * @returns {string} Una stringa che rappresenta il contenuto del libro.
+     */
+    @description("Metodo per ottenere il contenuto del libro come stringa")
+    toString() {
+      return `Titolo: ${this.titolo}, Autore: ${this.autore}, Genere: ${this.genere}, ISBN: ${this.isbn}`;
+    }
+  }
+  /**
+ * @classdesc Rappresenta una libreria virtuale con funzionalità per gestire libri, utenti e prestiti.
+ * @class
+ */
+class Libreria {
+  /**
+   * Crea un'istanza di Libreria e inizializza gli array per i libri, gli utenti e i prestiti.
+   * Carica i dati dei libri, degli utenti e dei prestiti se presenti.
+   * Crea le directory necessarie per la memorizzazione dei file, se non esistenti.
+   * @constructor
+   */
+  constructor() {
+    this.libri = [];
+    this.utenti = [];
+    this.prestiti = [];
+    const dir = path.join(os.homedir(), 'books');
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    this.filePath = path.join(dir, 'libreria.txt');
+    this.filePathUtenti = path.join(dir, 'utenti.txt');
+    this.prestitiFilePath = path.join(os.homedir(), 'books', 'prestiti.txt');
+    this.caricaLibri();
+    this.caricaUtenti();
+    this.caricaPrestiti();
+  }
+
+  /**
+  * @property {function} caricaLibri i libri memorizzati da un file nella libreria. Se il file non esiste, crea la directory e il file necessari.
+  * @returns {void}
+  **/ 
+
+  caricaLibri() {
+    if (fs.existsSync(this.filePath)) {
+      const data = fs.readFileSync(this.filePath, 'utf8').trim();
+      if (data !== '') {
+        const lines = data.split('\n');
+        lines.forEach(line => {
+          const [titolo, autore, genere, isbn] = line.split(', ');
+          const libro = new Libro(titolo, autore, genere, isbn);
+          this.libri.push(libro);
+        });
+      }
+    } else {
+      const dir = path.join(os.homedir(), 'books');
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+    }
+  }
+
+  /**
+   * @property {function} salvaLibri Salva i libri nella libreria memorizzandoli su file.
+   * @returns {void}
+   */
+  salvaLibri() {
+    const lines = this.libri.map(libro => `${libro.titolo}, ${libro.autore}, ${libro.genere}, ${libro.isbn}`);
+    const data = lines.join('\n');
+    fs.writeFileSync(this.filePath, data, 'utf8');
+    console.log(`Libri salvati in ${this.filePath}`);
+}
+
+
+/**
+   * @property {function} aggiungiLibro Aggiunge un nuovo libro alla libreria.
+   * @returns {void}
+   */
+  aggiungiLibro() {
+    const titolo = prompt('Inserisci il titolo del libro: ');
+    const autore = prompt('Inserisci l\'autore del libro: ');
+    const genere = prompt('Inserisci il genere del libro: ');
+    const isbn = prompt('Inserisci l\'ISBN del libro: ');
+
+    const libroEsistente = this.libri.find(libro => libro.isbn === isbn);
+    if (libroEsistente) {
+      console.log("Il libro con lo stesso ISBN è già presente nella libreria.");
+      return;
+    }
+    const nuovoLibro = new Libro(titolo, autore, genere, isbn);
+    this.libri.push(nuovoLibro);
+    this.salvaLibri();
+    console.log("Libro aggiunto con successo.");
+  }
+
+
+/**
+   * @property {function} visualizzaLibri Visualizza tutti i libri presenti nella libreria.
+   * @returns {void}
+   */
+  visualizzaLibri() {
+    this.libri.forEach(libro => {
+      console.log(libro.toString());
+    });
+  }
+
+  /**
+  * @property {function} aggiornaLibro Aggiorna le informazioni di un libro esistente nella libreria.
+  * @returns {void}
+  */
+  aggiornaLibro() {
+    const isbn = prompt('Inserisci l\'ISBN del libro da aggiornare: ');
+    const nuovoTitolo = prompt('Inserisci il nuovo titolo del libro: ');
+    const nuovoAutore = prompt('Inserisci il nuovo autore del libro: ');
+    const nuovoGenere = prompt('Inserisci il nuovo genere del libro: ');
+
+    const libroDaAggiornare = this.libri.find(libro => libro.isbn === isbn);
+    if (libroDaAggiornare) {
+      libroDaAggiornare.titolo = nuovoTitolo;
+      libroDaAggiornare.autore = nuovoAutore;
+      libroDaAggiornare.genere = nuovoGenere;
+      this.salvaLibri();
+      console.log("Informazioni del libro aggiornate con successo.");
+    } else {
+      console.log("Il libro non è presente nella libreria.");
+    }
+  }
+
+  /**
+   * @property {function} ricercaAvanzata Esegue una ricerca avanzata all'interno della libreria in base al termine inserito dall'utente. La ricerca viene effettuata sui titoli, gli autori e i generi dei libri.
+   * @returns {void}
+   */
+  ricercaAvanzata() {
+    const ricerca = prompt('Inserisci il termine di ricerca: ').toLowerCase();
+    const risultati = this.libri.filter(libro =>
+      libro.titolo.toLowerCase().includes(ricerca) ||
+      libro.autore.toLowerCase().includes(ricerca) ||
+      libro.genere.toLowerCase().includes(ricerca)
+    );
+
+    if (risultati.length === 0) {
+      console.log("Nessun libro trovato corrispondente alla ricerca.");
+    } else {
+      console.log("Risultati della ricerca:");
+      risultati.forEach(libro => {
+        console.log(libro.toString());
+      });
+    }
+  }
+
+  /** 
+   * @property {function} salvaUtenti Salva gli utenti della libreria su file.
+   * @returns {void}
+   */
+
+  salvaUtenti() {
+    const lines = this.utenti.map(utente => `${utente.id}, ${utente.nome}`);
+    const data = lines.join('\n');
+    fs.writeFileSync(this.filePathUtenti, data, 'utf8');
+    console.log(`Utenti salvati in ${this.filePathUtenti}`);
+  }
+
+
+
+  /**
+ * 
+ * @property {function} caricaUtenti Carica gli utenti dalla memoria persistente.
+ * @memberof Libreria
+ * @returns {void}
+ */
+caricaUtenti() {
+  if (fs.existsSync(this.filePathUtenti)) {
+    const data = fs.readFileSync(this.filePathUtenti, 'utf8').trim();
+    if (data !== '') {
+      const lines = data.split('\n');
+      lines.forEach(line => {
+        const [id, nome] = line.split(', ');
+        const utente = { id: parseInt(id), nome };
+        this.utenti.push(utente);
+      });
+    }
+  }
+}
+/**
+   * 
+   * @property {function} aggiungiUtente Aggiunge un nuovo utente alla libreria.
+   * @memberof Libreria
+   * @returns {void}
+   */
+  aggiungiUtente(nome) {
+    const id = this.utenti.length + 1;
+    const nuovoUtente = { id, nome };
+    this.utenti.push(nuovoUtente);
+    this.salvaUtenti();
+    console.log(`Utente ${nome} aggiunto con ID ${id}.`);
+  }
+
+  /**
+   * 
+   * @property {function} visualizzaUtenti Visualizza l'elenco degli utenti presenti nella libreria.
+   * @memberof Libreria
+   * @returns {void}
+   */
+  visualizzaUtenti() {
+    console.log("Elenco degli utenti:");
+    this.utenti.forEach(utente => {
+      console.log(`ID: ${utente.id}, Nome: ${utente.nome}`);
+    });
+  }
+
+   /**
+   * 
+   * @property {function} effettuaPrestito Effettua un prestito di un libro a un utente.
+   * @memberof Libreria
+   * @param {string} isbn - L'ISBN del libro da prestare.
+   * @param {number} userId - L'ID dell'utente a cui prestare il libro.
+   * @returns {void}
+   */
+   effettuaPrestito(isbn, userId) {
+    const libroPrestato = this.prestiti.find(prestito => prestito.isbn === isbn);
+    if (libroPrestato) {
+      console.log("Questo libro è già stato prestato.");
+      return;
+    }
+
+    const libro = this.libri.find(libro => libro.isbn === isbn);
+    const utente = this.utenti.find(utente => utente.id === userId);
+    if (!libro || !utente) {
+      console.log("Libro o utente non trovato.");
+      return;
+    }
+    const prestito = { isbn, userId, dataPrestito: new Date() };
+    this.prestiti.push(prestito);
+    this.salvaPrestiti();
+    console.log(`Prestito effettuato: Libro ISBN ${isbn} a Utente ${utente.nome}`);
+  }
+
+  
+
+  /**
+ * 
+ * @property {function} caricaPrestiti Carica i dati dei prestiti dalla memoria persistente.
+ * @memberof Libreria
+ * @returns {void}
+ */
+caricaPrestiti() {
+  if (fs.existsSync(this.prestitiFilePath)) {
+    const data = fs.readFileSync(this.prestitiFilePath, 'utf8').trim();
+    if (data !== '') {
+      const lines = data.split('\n');
+      lines.forEach(line => {
+        const [userId, isbn, dataPrestito] = line.split(', ');
+        const prestito = { userId: parseInt(userId), isbn, dataPrestito: new Date(dataPrestito) };
+        this.prestiti.push(prestito);
+      });
+    }
+  }
+}
+
+  /**
+   * 
+   * @property {function} visualizzaPrestiti Visualizza l'elenco dei prestiti effettuati.
+   * @memberof Libreria
+   * @returns {void}
+   */
+  visualizzaPrestiti() {
+    console.log("Elenco dei prestiti:");
+    this.prestiti.forEach(prestito => {
+      const libro = this.libri.find(libro => libro.isbn === prestito.isbn);
+      const utente = this.utenti.find(utente => utente.id === prestito.userId);
+      console.log(`Utente: ${utente.nome}, Libro: ${libro.titolo}, Data prestito: ${prestito.dataPrestito}`);
+    });
+  }
+
+
+  /**
+ * @property {function} restituisciLibro  Restituisce un libro precedentemente prestato da un utente.
+ * @memberof Libreria
+ * @param {number} userId - L'ID dell'utente che restituisce il libro.
+ * @param {string} isbn - L'ISBN del libro da restituire.
+ * @returns {void}
+ */
+restituisciLibro(userId, isbn) {
+  const prestitoIndex = this.prestiti.findIndex(prestito => prestito.userId === userId && prestito.isbn === isbn);
+  if (prestitoIndex === -1) {
+    console.log("Prestito non trovato.");
+    return;
+  }
+  this.prestiti.splice(prestitoIndex, 1);
+  this.salvaPrestiti();
+  console.log("Libro restituito con successo.");
+}
+
+/**
+ * @property {function} salvaPrestiti Salva i dati dei prestiti su file.
+ * @memberof Libreria
+ * @returns {void}
+ */
+salvaPrestiti() {
+  const lines = this.prestiti.map(prestito => `${prestito.userId}, ${prestito.isbn}, ${prestito.dataPrestito}`);
+  const data = lines.join('\n');
+  fs.writeFileSync(this.prestitiFilePath, data, 'utf8');
+  console.log(`Prestiti salvati in ${this.prestitiFilePath}`);
+}
+
+
+  // Funzione per gestire il menu avanzato
+  menuAvanzato() {
+    let scelta;
+    do {
+      console.log("\nMenu Avanzato:");
+      console.log("1. Aggiungi libro");
+      console.log("2. Visualizza libri");
+      console.log("3. Aggiorna libro");
+      console.log("4. Ricerca avanzata");
+      console.log("5. Aggiungi utente");
+      console.log("6. Visualizza utenti");
+      console.log("7. Effettua prestito");
+      console.log("8. Visualizza prestiti");
+      console.log("9. Effettua una restituzione");
+      console.log("0. Esci");
+
+      scelta = prompt("Scelta: ");
+      switch (scelta) {
+        case '1':
+          this.aggiungiLibro();
+          break;
+        case '2':
+          this.visualizzaLibri();
+          break;
+        case '3':
+          this.aggiornaLibro();
+          break;
+        case '4':
+          this.ricercaAvanzata();
+          break;
+        case '5':
+          const nomeUtente = prompt('Inserisci il nome dell\'utente: ');
+          this.aggiungiUtente(nomeUtente);
+          break;
+        case '6':
+          this.visualizzaUtenti();
+          break;
+        case '7':
+          const isbn = prompt('Inserisci l\'ISBN del libro: ');
+          const userId = prompt('Inserisci l\'ID dell\'utente: ');
+          this.effettuaPrestito(isbn, parseInt(userId));
+          break;
+        case '8':
+          this.visualizzaPrestiti();
+          break;
+        case '9':
+          const userIdRestituzione = prompt('Inserisci l\'ID dell\'utente che restituisce il libro: ');
+          const isbnRestituzione = prompt('Inserisci l\'ISBN del libro da restituire: ');
+          this.restituisciLibro(parseInt(userIdRestituzione), isbnRestituzione);
+          break;
+          
+        case '0':
+          console.log("Uscita dal programma.");
+          break;
+        default:
+          console.log("Scelta non valida. Riprova.");
+      }
+    } while (scelta !== '0');
+  }
+}
+const miaLibreria = new Libreria();
+miaLibreria.menuAvanzato();
